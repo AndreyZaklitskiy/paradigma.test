3c7f11126bdf99a952e53a30ffd8cfce9922dbd1
AndreyZaklitskiy <glockman94@gmail.com>
2020-07-09 20:33:07 +0300

    last commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+$(function() {
+
 let pincet = {
     init() {
         for (let key in this) {
@@ -5,16 +7,22 @@ let pincet = {
         }
       },
 
-    PulsBtnOnHover() {
-        let text = $('.head__client-card-lorem');
-        let btn = $('.pulsation-btn');
-        btn.on('mouseover', function () {
-            text.addClass('visible');
+      discharge() {
+        $('a, button').on('click', () => {
+          event.preventDefault();
+        });
+      },
+
+      PulsBtnOnHover() {
+          let text = $('.head__client-card-lorem');
+          let btn = $('.pulsation-btn');
+          btn.on('mouseover', function () {
+              text.addClass('visible');
+          })
+          btn.on('mouseout', function () {
+            text.removeClass('visible');
         })
-        btn.on('mouseout', function () {
-          text.removeClass('visible');
-      })
-    },
+      },
 
       PopupButtonHandler() {
         $('.popup-form_btn span').text($('input[name="radio"]:checked').parent().next().text());
@@ -30,75 +38,68 @@ let pincet = {
           }
         });
       },
-      HamburgerHandler() {
+      MenuHandler() {
         let hamburger = $('.hamburger'),
             list      = $('.head__menu-list'),
-            item      = $('.head__menu-item');
+            item      = $('.head__menu-item ');
         hamburger.on('click', () => {
           hamburger.toggleClass('is-active');
-          list.slideDown();
-          if(hamburger.hasClass('is-active')){
-            list.slideUp();
-          }
+          list.slideToggle();
         })
-        item.on('click', () => {
-          hamburger.removeClass('is-active');
-          list.slideUp();
+        if ($(window).width() <= '1200') {
+          list.children('li').children('a').removeClass('active');
+          item.on('click', (e) => {
+            let self = $(e.target),
+              text = self.text();
+              $('ul > .head__menu-item').children('a').text(text);
+
+              console.log(text);
+              hamburger.removeClass('is-active');
+              list.slideUp();
+          })
+      }
+      },
+      PopupHendler() {
+        $('.popup-close').on('click', () => {
+          $('.popup').fadeOut();
+          $('.popup_wrap').fadeOut();
+        })
+        $('.open-popup').on('click', () => {
+          $('.popup_wrap').fadeIn();
+          $('.popup_get-program').fadeIn();
+        })
+        $('.popup-btn').on('click', () => {
+          $('.popup_get-program').fadeOut();
+          $('.popup_done').fadeIn();
         })
       },
-      MenuHandler() {
-        if ($(window).width() <= '1200') {
-          let list = $('.head__menu-list'),
-              item = $('.head__menu-item');
-          item.on('click', () => {
-            let self = $(this),
-              text = self.children('a').text();
-              $('.head__menu > .container ul > li > a').text(text);
-          })
 
-        }
+      FormValidation() {
+        $("#phone").inputmask("+7 (999) 99-99-999");
+        $(".popup-form").validate();
+        $.validator.addClassRules({
+          'input-name': {
+            required: true,
+            minlength: 2
+          },
+          'input-phone': {
+            required: true,
+            minlength: 9,
+            maxlength: 18
+          }
+        });
+
+        $(".input-phone").on("blur", () => {
+          if($('.popup-form').valid()){
+              $(".popup-btn").removeAttr("disabled");
+         }
+        });
+      },
+      ConnectionRellaxJs() {
+        var rellax = new Rellax('.rellax');
       }
+      
 
 }.init();
 
-$(function() {
-  $('a, button').on('click', () => {
-    event.preventDefault();
-  });
-
-  var rellax = new Rellax('.rellax');
-
-  $("#phone").inputmask("+7 (999) 99-99-999");
-  $(".popup-form").validate();
-   $.validator.addClassRules({
-     'input-name': {
-       required: true,
-       minlength: 2
-     },
-     'input-phone': {
-       required: true,
-
-       minlength: 9,
-       maxlength: 18
-     }
-   });
-
-  $(".input-phone").on("blur", () => {
-    if($('.popup-form').valid()){
-        $(".popup-btn").removeAttr("disabled");
-   }
-  });
-
-  $('.popup-close').on('click', () => {
-    $('.popup').fadeOut();
-    $('.popup_wrap').fadeOut();
-  })
-  $('.open-popup').on('click', () => {
-    $('.popup_wrap').fadeIn();
-    $('.popup_get-program').fadeIn();
-  })
-  $('.popup-btn').on('click', () => {
-    $('.popup_get-program').fadeOut();
-    $('.popup_done').fadeIn();
-  })
 });
